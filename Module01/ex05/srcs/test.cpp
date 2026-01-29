06a985ca6e915d39d4dccc70ba2828d15befe0fa
ceske-42 <ceske@etik.com>
2026-01-29 16:35:45 +0100

    improved test output

--- Module01/ex05/srcs/test.cpp
+++ Module01/ex05/srcs/test.cpp
@@ -1,11 +1,15 @@
 #include "Harl.class.hpp"
+#include <iostream>
 
 int main(void)
 {
 	Harl harl;	
 
 	harl.complain("DEBUG");
+	std::cout << std::endl;
 	harl.complain("INFO");
+	std::cout << std::endl;
 	harl.complain("WARNING");
+	std::cout << std::endl;
 	harl.complain("ERROR");
 }
