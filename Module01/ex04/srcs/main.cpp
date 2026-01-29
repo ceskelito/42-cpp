83f49e4bdced9081f479eefc32f2d57abbee2cbf
ceske-42 <ceske@etik.com>
2026-01-29 12:41:50 +0100

    Sed works fine

--- Module01/ex04/srcs/main.cpp
+++ Module01/ex04/srcs/main.cpp
@@ -7,30 +7,22 @@ using std::string;
 using std::ifstream;
 using std::ofstream;
 
-int substitute(ifstream &src, ofstream &tgt, const string s1, const string s2) {
-	using std::getline;
-
+void	sed(ifstream &src, ofstream &tgt, const string s1, const string s2) {
 	string				buff;
-	string::size_type	prev = 0; 
-	int					count = 0;
+	string::size_type	pos = 0;
+	bool				flag = false;
 	
-	getline(src, buff);
-
-	for (string::size_type p = buff.find(s1); p != string::npos; p = buff.find(s1[prev])) {
-		tgt << buff.substr(prev, p - 1);
-		tgt << s2;
-		prev = p + s2.length();
-		if (p > buff.length())
-			break ;
-		count++;
+	
+	while (getline(src, buff)) {
+		if (flag)
+			tgt << '\n';
+		flag = true;
+		while (pos = buff.find(s1), pos != string::npos) {
+			tgt << buff.substr(0, pos) << s2;
+			buff = buff.substr(pos + s1.length());
+		}
+		tgt << buff;
 	}
-	tgt << buff.substr(prev, buff.length());
-	
-	// if (buff.find(s1) == string::npos)
-	// 	std::cout << "POS: npos";
-	// else
-	// 	std::cout << "POS: " << buff.find(s1);
-	return count;
 }
 
 int main(int ac, char **av) {
@@ -48,7 +40,10 @@ int main(int ac, char **av) {
 	ofstream		target;
 
 	source.open(filename.c_str());
+	if (!source.is_open()) {
+		std::cout << "Failed opening the file `" << filename << '\'' << std::endl;
+		return (1);
+	}
 	target.open((filename + ".replace").c_str());
-
-	substitute(source, target, s1, s2);
+	sed(source, target, s1, s2);
 }
