894d125a07f97fbb6ab5aec1c9736faf04c599b9
ceske-42 <ceske@etik.com>
2026-02-12 14:11:42 +0100

    Copied updated ClapTrap files from previous exercise

--- Module03/ex01/ClapTrap.class.cpp
+++ Module03/ex01/ClapTrap.class.cpp
@@ -4,62 +4,92 @@
 using std::cout;
 using std::endl;
 
-ClapTrap::ClapTrap(void) : 
-	_name("defaultName"),
-	_hitPoints(10),
-	_energyPoints(10),
-	_attackDamage(0) {
-	
-	cout << "ClapTrap void constructor called" << endl;
-}
+// Constructors / Destructor
 
 ClapTrap::ClapTrap(string name) : 
 	_name(name),
 	_hitPoints(10),
 	_energyPoints(10),
 	_attackDamage(0) {
-	
-	cout << "ClapTrap parametrized constructor called" << endl;
+
+	cout << "[ ClapTrap ] - " << _name << " has been born." << endl;
 }
 
 ClapTrap::~ClapTrap(void) {
-	cout << "ClapTrap destructor called" << endl;
+	cout << "[ ClapTrap ] - " << _name << " has been destroyed." << endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other) :
+	_name(other._name),
+    _hitPoints(other._hitPoints),
+    _energyPoints(other._energyPoints),
+    _attackDamage(other._attackDamage) {
+
+    cout << "[ ClapTrap ] - " << _name << " has been copied with copy constructor." << endl;
+}
+
+ClapTrap&   ClapTrap::operator=(const ClapTrap& other) {
+    _name			= other._name;
+    _hitPoints		= other._hitPoints;
+    _energyPoints	= other._energyPoints;
+    _attackDamage	= other._attackDamage;
+
+    cout << "[ ClapTrap ] - " << _name << " has been copied with copy assignment operator." << endl;
+    return (*this);
 }
 
 // Private Member Functions
 
-void	ClapTrap::_subEnergy(unsigned int amount) {
+void	ClapTrap::_subEnergy(unsigned int amount = 1) {
 	_energyPoints -= amount;
 }
 
-void	ClapTrap::_subHit(unsigned int amount) {
+void	ClapTrap::_subHit(unsigned int amount = 1) {
 	_hitPoints -= amount;
 }
 
-void	ClapTrap::_gainEnergy(unsigned int amount) {
+void	ClapTrap::_gainEnergy(unsigned int amount = 1) {
 	_energyPoints += amount;
 }
 
-void	ClapTrap::_gainHit(unsigned int amount) {
+void	ClapTrap::_gainHit(unsigned int amount = 1) {
 	_hitPoints += amount;
 }
 
 // Public Member Functions
 
-void	ClapTrap::attack(const string &target) {
-	_subEnergy(1);
-	cout << "ClapTrap " << _name << " attacks " << target <<
-		", causing " << _attackDamage << " points of damage!" << endl;
+void    ClapTrap::attack( std::string const& target )
+{
+    if (_energyPoints <= 0)
+    {
+        cout << "[ ClapTrap ] - " << _name << " is out of energy! It cannot attack." << endl;
+        return;
+    }
+    cout << "[ ClapTrap ] - " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << endl;
+	_subEnergy();
 }
 
-void	ClapTrap::takeDamage(unsigned int amount) {
+void    ClapTrap::takeDamage( unsigned int amount )
+{
+    cout << "[ ClapTrap ] - " << _name << " takes " << amount << " points of damage!" << endl;
+
+    if (_hitPoints <= amount)
+    {
+        cout << "[ ClapTrap ] - " << _name << " is dead!" << endl;
+		_subHit(_hitPoints);
+        return;
+    }
 	_subHit(amount);
-	cout << "ClapTrap " << _name << " has be attacked " <<
-		", taking " << amount << " points of damage!" << endl;
 }
 
-void	ClapTrap::beRepaired(unsigned int amount) {
-	_subEnergy(1);
-	cout << "ClapTrap " << _name << " repaired itself " <<
-		", gaining " << amount << " hit points!" << endl;
+void    ClapTrap::beRepaired( unsigned int amount )
+{
+    if (_energyPoints < amount)
+    {
+        cout << "[ ClapTrap ] - " << _name << " is out of energy! It cannot be repaired." << endl;
+        return;
+    }
+    cout << "[ ClapTrap ] - " << _name << " repairs itself, gaining " << amount << " hit points!" << endl;
+	_subEnergy();
+	_gainHit(amount);
 }
--- Module03/ex01/ClapTrap.class.hpp
+++ Module03/ex01/ClapTrap.class.hpp
@@ -6,24 +6,28 @@ using std::string;
 
 class ClapTrap {
 
-protected:
-	string	_name;
-	int		_hitPoints;
-	int		_energyPoints;
-	int		_attackDamage;
+private:
+	string			_name;
+	unsigned int	_hitPoints;
+	unsigned int	_energyPoints;
+	unsigned int	_attackDamage;
 
 	void	_subEnergy(unsigned int amount);
 	void	_subHit(unsigned int amount);
 
 	void	_gainEnergy(unsigned int amount);
 	void	_gainHit(unsigned int amount);
+	
+	ClapTrap(void);
 
 public:
 
 	// Constructors / Destructor
-	ClapTrap(void);
 	ClapTrap(string name);
 	~ClapTrap(void);
+
+	ClapTrap(const ClapTrap &other);
+	ClapTrap	&operator=(const ClapTrap &other);
 
 	// Member functions
 	void	attack(const string &target);
