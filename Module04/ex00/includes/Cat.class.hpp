818132aaab5e4e99a847479d1e024d54e8dbd496
ceske-42 <ceske@etik.com>
2026-02-16 13:15:16 +0100

    working on

--- Module04/ex00/includes/Cat.class.hpp
+++ Module04/ex00/includes/Cat.class.hpp
@@ -2,21 +2,18 @@
 
 #include "Animal.class.hpp"
 
-#include <string>
-
 class Cat: virtual public Animal {
 
 private:
 	using	Animal::_announceMethod;
 
-	static const std::string	_className;
-
 public:
+	using Animal::operator=;
+	
 	Cat( void );
 	~Cat( void );
-	//
-	// Cat( Cat &other );
-	using Animal::operator=;
+	Cat( Cat &other );
+	
 
 	void	makeSound( void );
 };
