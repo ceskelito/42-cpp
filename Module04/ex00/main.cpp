32a5706f16e5c2a94f3152e052483454b2529680
ceske-42 <ceske@etik.com>
2026-02-16 14:49:36 +0100

    Finished

--- Module04/ex00/main.cpp
+++ Module04/ex00/main.cpp
@@ -1,17 +1,23 @@
 #include "Cat.class.hpp"
+#include "Dog.class.hpp"
+
+#include <iostream>
 
 int main( void ) {
 	Animal	*meta = new Animal();
 	Cat		*cat = new Cat();
+	Dog		*dog = new Dog();
 
 	cat->getType();
 	cat->makeSound();
+	std::cout << std::endl;
 	meta->getType();
 	meta->makeSound();
-
-	Cat newCat = *cat;
-	newCat.makeSound();
+	std::cout << std::endl;
+	dog->getType();
+	dog->makeSound();
 
 	delete cat;
+	delete dog;
 	delete meta;
 }
