57ef3a6eca07273b0539f6614b91dc48fa752b87
ceske-42 <ceske@etik.com>
2026-02-16 17:04:09 +0100

    Improved Brain _announceMethod output

--- Module04/ex01/srcs/Brain.class.cpp
+++ Module04/ex01/srcs/Brain.class.cpp
@@ -1,9 +1,16 @@
 #include "Brain.class.hpp"
 
 #include <iostream>
+#include <iomanip>
 
 void	Brain::_announceMethod( std::string methodName) {
-	std::cout << "# Brain #" << methodName << " Called." << std::endl;
+	std::string	className = "Brain";
+	int			delta = 10 - className.length();
+	int			wid1 = delta / 2;
+	int			wid2 = (delta % 2 == 0) ? wid1 : wid1 + 1;
+
+	std::cout << std::setw(wid1) << std::left << "#" << className << std::setw(wid2) << std::right <<  "#";
+	std::cout << " - " << methodName << " Called." << std::endl;
 }
 
 Brain::Brain( Brain &other ) {
