724bf084d875cf53020b524d0181189dc4b30140
ceske-42 <ceske@etik.com>
2026-02-17 12:04:54 +0100

    Improved main output

--- Module04/ex01/main.cpp
+++ Module04/ex01/main.cpp
@@ -12,55 +12,58 @@ int main( void ) {
 
 	Animal *animals[e_maxAnimals];
 
-	if (e_maxAnimals % 2 != 0) {
-		cout << "Please set a pair numer of animals" << endl;
-		return 1;
-	}
-
 	cout << "Constructing animals..." << endl;
-	cout << "- - -" << endl;
+	cout << "- - - - - -" << endl;
 	for (int i = 0; i < e_maxAnimals; i++) {
 		if (i % 2)
 			animals[i] = new Cat();
 		else 
 			animals[i] = new Dog();
-		cout << "- - -" << endl;
+		cout << "- - - - - -" << endl;
 	}
-	
 	cout << endl;
+
+	cout << "Let's listen the animals sounds..." << endl;
+	cout << "- - - - - -";
 	for (int i = 0; i < e_maxAnimals; i++) {
+		cout << endl;
 		cout << animals[i]->getType() << ": ";
 		animals[i]->makeSound();
-		cout << endl;
 	}
+	cout << "- - - - - -" << endl << endl;
 
 	Animal *copies[e_maxAnimals];
 
 	cout << "Creating copies of animals..." << endl;
-	for (int i = 0; i < e_maxAnimals; i++) {
-		// copies[i] = new Animal(*animals[i]);
-	}
-
-	// Demostrating that copies are deep copies
-	cout << endl;
+	cout << "- - - - - -" << endl;
 	for (int i = 0; i < e_maxAnimals; i++){
+		copies[i] = animals[i]->clone();
+		cout << "- - - - - -" << endl;
+	}
+	cout << endl;
+
+	cout << "Demostrating that copies are deep copies..." << endl;
+	cout << "- - - - - -";
+	for (int i = 0; i < e_maxAnimals; i++){
+		cout << endl;
 		cout << "Index [" << i << "]" << endl;
 		cout << "Original memory address: " << animals[i] << endl;
-		cout << "Copy     memory address: " << copies[i] << endl;
-		cout << endl;
+		cout << "Copy   memory   address: " << copies[i] << endl;
 	}
+	cout << "- - - - - -" << endl << endl;
 
 	cout << "Deliting originals..." << endl;
-	cout << "- - -" << endl;
+	cout << "- - - - - -" << endl;
 	for (int i = 0; i < e_maxAnimals; i++) {
 		delete animals[i];
-		cout << "- - -" << endl;
+		cout << "- - - - - -" << endl;
 	}
 	cout << endl;
+
 	cout << "Deliting copies..." << endl;
-	cout << "- - -" << endl;
+	cout << "- - - - - -" << endl;
 	for (int i = 0; i < e_maxAnimals; i++) {
 		delete copies[i];
-		cout << "- - -" << endl;
+		cout << "- - - - - -" << endl;
 	}
 }
