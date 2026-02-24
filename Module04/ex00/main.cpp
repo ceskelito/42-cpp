31968fec588127c1e64b14d48ecd3b292cfad345
ceske-42 <ceske@etik.com>
2026-02-24 14:44:43 +0100

    Added tests for Wrong* classes

--- Module04/ex00/main.cpp
+++ Module04/ex00/main.cpp
@@ -1,9 +1,14 @@
 #include "Cat.class.hpp"
 #include "Dog.class.hpp"
 
+#include "WrongCat.class.hpp"
+#include "WrongDog.class.hpp"
+
 #include <iostream>
 
 int main( void ) {
+	std::cout << "Correct Animals test suite..." << std::endl << std::endl;
+
 	Animal	*meta = new Animal();
 	Animal	*cat = new Cat();
 	Animal	*dog = new Dog();
@@ -28,4 +33,32 @@ int main( void ) {
 	delete cat;
 	delete dog;
 	delete meta;
+
+	std::cout << std::endl << "#####################################" << std::endl << std::endl;
+	std::cout << "Wrong Animals test suite..." << std::endl << std::endl;
+
+	WrongAnimal	*w_meta = new WrongAnimal();
+	WrongAnimal	*w_cat = new WrongCat();
+	WrongAnimal	*w_dog = new WrongDog();
+
+	std::cout << std::endl;
+	
+	w_cat->getType();
+	w_cat->makeSound();
+
+	std::cout << std::endl;
+
+	w_meta->getType();
+	w_meta->makeSound();
+
+	std::cout << std::endl;
+
+	w_dog->getType();
+	w_dog->makeSound();
+	
+	std::cout << std::endl;
+
+	delete w_cat;
+	delete w_dog;
+	delete w_meta;
 }
