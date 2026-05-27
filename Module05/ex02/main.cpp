3c8520c3bac3b2a84433a6209cbd8754909684ef
Riccardo Ceschel <rceschel@student.42roma.it>
2026-05-27 17:35:41 +0200

    simplified main header() function

--- Module05/ex02/main.cpp
+++ Module05/ex02/main.cpp
@@ -6,52 +6,28 @@
 using std::cout;
 using std::endl;
 
-struct HeaderManip {
-    std::ostream& apply(std::ostream& os) const {
-        os << BLK << WHTB;
-        return os;
-    }
-};
-
-// Proxy che cattura il testo successivo e aggiunge il reset
-struct HeaderProxy {
-    std::ostream& os;
-
-    HeaderProxy(std::ostream& os) : os(os) {}
-
-    // Cattura qualsiasi valore, lo stampa colorato, poi resetta
-    template <typename T>
-    std::ostream& operator<<(const T& val) {
-        os << val << CRESET;
-        return os;
-    }
-};
-
-// operator<< tra ostream e HeaderManip → restituisce HeaderProxy
-HeaderProxy operator<<(std::ostream& os, const HeaderManip&) {
-    os << BLK << WHTB;
-    return HeaderProxy(os);
-}
-
-HeaderManip header() {
-    return HeaderManip();
+static string header(string const text) {
+	return ( string( BLK ) + WHTB + text + CRESET );
 }
 
 int main(void)
 {
-	cout << header() << "Creating a Form:" <<  endl;
+	cout << header("Creating a Form:") <<  endl;
 	ShrubberyCreationForm	first("BeatifulTree");
 	cout << endl;
 
-	cout << header() << "Creating a copy from the first Form:" << endl;
+	cout << header("Creating a copy from the first Form:") << endl;
 	ShrubberyCreationForm	second(first);
 	cout << endl;
 
-	cout << header() << "Creating a Bureaucrat:" << endl;
+	cout << header("Creating a Bureaucrat:") << endl;
 	Bureaucrat				b("Bobby", 10);
 	cout << endl;
 
-	cout << header() << "Executing some Forms..." << endl;
+	cout << header("Executing some Forms...") << endl;
 	first.execute(b);
+
+	cout << endl;
+	cout << header("End of the program, automatic calling destructors chain.") << endl;
 	return 0;
 }
