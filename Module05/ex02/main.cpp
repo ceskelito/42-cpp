ed4c2a568426f62e1ca64cc4286bd454ce33c311
Riccardo Ceschel <rceschel@student.42roma.it>
2026-05-27 16:05:17 +0200

    Improved Readability for main output

--- Module05/ex02/main.cpp
+++ Module05/ex02/main.cpp
@@ -1,12 +1,24 @@
 #include "Bureaucrat.class.hpp"
 #include "ShrubberyCreationForm.class.hpp"
+#include "AnsiColorCodes.h"
 #include <iostream>
+
+using std::cout;
+using std::endl;
 
 int main(void)
 {
-	ShrubberyCreationForm	first("target");
+	cout << BLU << "Creating a Form:" << WHT << endl;
+	ShrubberyCreationForm	first("BeatifulTree");
+	cout << endl;
+
+	cout << BLU << "Creating a copy from the first Form:" << WHT << endl;
 	ShrubberyCreationForm	second(first);
-	Bureaucrat				b("name", 10);
+	cout << endl;
+
+	cout << BLU << "Creating a Bureaucrat:" << WHT << endl;
+	Bureaucrat				b("Bobby", 10);
+	cout << endl;
 
 	first.execute(b);
 	return 0;
