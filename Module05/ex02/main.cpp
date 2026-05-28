b69d952680a180ad79e3b25b3fd8773cd2fced3e
Riccardo Ceschel <rceschel@student.42roma.it>
2026-05-28 15:36:46 +0200

    Improved main

--- Module05/ex02/main.cpp
+++ Module05/ex02/main.cpp
@@ -6,28 +6,52 @@
 using std::cout;
 using std::endl;
 
-static string header(string const text) {
-	return ( string( BLK ) + WHTB + text + CRESET );
+// static string header(string const text) {
+// 	return ( string( BLK ) + WHTB + text + CRESET );
+// }
+
+static void printHeader (
+		string const& text,
+		string const background = WHTB,
+		string const color = BLK,
+		std::ostream &os = std::cout) {
+
+    os << string(color) + background + text + CRESET << std::endl;
 }
 
 int main(void)
 {
-	cout << header("Creating a Form:") <<  endl;
+	
+	printHeader("SECTION 1: ShrubberyCreationForm", BLUB, WHT);
+	cout << endl;
+
+	printHeader("Creating a Form:");
 	ShrubberyCreationForm	first("BeatifulTree");
 	cout << endl;
 
-	cout << header("Creating a copy from the first Form:") << endl;
+	printHeader("Creating a copy from the first Form:");
 	ShrubberyCreationForm	second(first);
 	cout << endl;
 
-	cout << header("Creating a Bureaucrat:") << endl;
+	printHeader("Printing forms informations:");
+	cout << endl;
+
+	printHeader("First", GRNB, WHT);
+	cout << first << endl;
+	cout << endl;
+
+	printHeader("Copy of first", GRNB, WHT);
+	cout << second << endl;
+	cout << endl;
+
+	printHeader("Creating a Bureaucrat:");
 	Bureaucrat				b("Bobby", 10);
 	cout << endl;
 
-	cout << header("Executing some Forms...") << endl;
+	printHeader("Executing some Forms...");
 	first.execute(b);
 
 	cout << endl;
-	cout << header("End of the program, automatic calling destructors chain.") << endl;
+	printHeader("End of the program, automatic calling destructors chain.");
 	return 0;
 }
