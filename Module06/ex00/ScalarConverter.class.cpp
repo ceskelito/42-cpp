5e5dc9c29cd3b6e2de1b4af0b5434875c0c1f507
Riccardo Ceschel <rceschel@student.42roma.it>
2026-06-02 17:56:19 +0200

    Identified the problem source - working on the fix

--- Module06/ex00/ScalarConverter.class.cpp
+++ Module06/ex00/ScalarConverter.class.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cstdlib> // Ascii to x conversions
 #include <errno.h>
+#include <sstream>
+#include <limits.h>
 
 typedef enum { CHAR, INT, FLOAT, DOUBLE, UNDEFINED } e_type;
 
@@ -18,6 +20,7 @@ static bool	isInt( std::string l ) {
 	return true;
 }
 
+// TODO manage the point at the very end
 static bool	isFloat( std::string l ) {
 
 	bool		hasDecimal = false;
@@ -42,6 +45,7 @@ static bool	isFloat( std::string l ) {
 	return true;
 }
 
+// TODO manage the point at the very end
 static bool isDouble( std::string l ) {
 
 	bool		hasDecimal = false;
@@ -78,33 +82,40 @@ static e_type identify_type ( std::string l) {
 		return UNDEFINED;
 }
 
-#include <sstream>
 template <e_type Type>
-static std::string testToString(void *ptr);
+static std::string toString(void *data);
 
 template <>
-std::string testToString<CHAR>(void *ptr) {
-    std::ostringstream oss;
-    char value = *static_cast<char*>(ptr);
-    if (!std::isprint(value))
-        oss << "not displayable";
-    else
-        oss << "'" << value << "'";
+std::string toString<CHAR>(void *data) {
+
+    std::ostringstream	oss;
+	int					value = *static_cast<int*>(data);
+
+	if ( value < 0 || value > 127 )
+		oss << "impossible";
+	else if (!std::isprint(value))
+		oss << "not displayable";
+	else
+		oss << "'" << static_cast<char>(value) << "'";
     return oss.str();
 }
 
 template <>
-std::string testToString<INT>(void *ptr) {
-    std::ostringstream oss;
-    int value = *static_cast<int*>(ptr);
+std::string toString<INT>(void *data) {
+
+    std::ostringstream	oss;
+    int					value = *static_cast<int*>(data);
+
     oss << value;
     return oss.str();
 }
 
 template <>
-std::string testToString<FLOAT>(void *ptr) {
-    std::ostringstream oss;
-    float value = *static_cast<float*>(ptr);
+std::string toString<FLOAT>(void *data) {
+
+    std::ostringstream	oss;
+    float				value = *static_cast<float*>(data);
+
     oss << value;
     if (value - static_cast<int>(value) == 0)
         oss << ".0";
@@ -113,207 +124,174 @@ std::string testToString<FLOAT>(void *ptr) {
 }
 
 template <>
-std::string testToString<DOUBLE>(void *ptr) {
+std::string toString<DOUBLE>(void *data) {
     std::ostringstream oss;
-    double value = *static_cast<double*>(ptr);
+    double value = *static_cast<double*>(data);
     oss << value;
     if (value - static_cast<int>(value) == 0)
         oss << ".0";
     return oss.str();
 }
 
-template <typename T>
-static std::string toString(const T& value) {
-    std::ostringstream oss;
-    oss << value;
-    return oss.str();
+// template <typename T>
+// static std::string toString(const T& value) {
+//     std::ostringstream oss;
+//     oss << value;
+//     return oss.str();
+// }
+//
+// template <>
+// std::string toString<float>(const float& value) {
+//     std::ostringstream oss;
+//     oss << value;
+// 	if (value - static_cast<int> ( value ) == 0)
+// 		oss << ".0";
+// 	oss << "f";
+//     return oss.str();
+// }
+//
+// template <>
+// std::string toString<double>(const double& value) {
+//     std::ostringstream oss;
+//     oss << value;
+// 	if (value - static_cast<int> ( value ) == 0)
+// 		oss << ".0";
+//     return oss.str();
+// }
+//
+// template <>
+// std::string toString<char>(const char& value) {
+//     std::ostringstream oss;
+// 	if (!std::isprint(value))
+// 		oss << "not displayable";
+// 	else
+// 		oss << "'" << value << "'";
+//     return oss.str();
+// }
+//
+
+// Helper function to extract value as double from any type
+static double getDoubleValue(e_type type, void *ref) {
+    switch(type) {
+        case CHAR: return static_cast<double>(*static_cast<char*>(ref));
+        case INT: return static_cast<double>(*static_cast<int*>(ref));
+        case FLOAT: return static_cast<double>(*static_cast<float*>(ref));
+        case DOUBLE: return *static_cast<double*>(ref);
+        default: return 0.0;
+    }
 }
 
-template <>
-std::string toString<float>(const float& value) {
-    std::ostringstream oss;
-    oss << value;
-	if (value - static_cast<int> ( value ) == 0)
-		oss << ".0";
-	oss << "f";
-    return oss.str();
+template <e_type Type>
+static void*	cast(e_type srcType, void *ref);
+
+template<>
+void	*cast<CHAR>(e_type srcType, void *ref) {
+    return new char(static_cast<char>(getDoubleValue(srcType, ref)));
 }
 
-template <>
-std::string toString<double>(const double& value) {
-    std::ostringstream oss;
-    oss << value;
-	if (value - static_cast<int> ( value ) == 0)
-		oss << ".0";
-    return oss.str();
+template<>
+void*	cast<INT>(e_type srcType, void* ref) {
+    return new int(static_cast<int>(getDoubleValue(srcType, ref)));
 }
 
-template <>
-std::string toString<char>(const char& value) {
-    std::ostringstream oss;
-	if (!std::isprint(value))
-		oss << "not displayable";
-	else
-		oss << "'" << value << "'";
-    return oss.str();
+template<>
+void*	cast<FLOAT>(e_type srcType, void* ref) {
+    return new float(static_cast<float>(getDoubleValue(srcType, ref)));
 }
-
-// Funzioni wrapper non-template per ogni tipo
-void* castToInt(void* ref) {
-    return new int(static_cast<int>(*static_cast<int*>(ref)));
-}
-
-void* castToFloat(void* ref) {
-    return new float(static_cast<float>(*static_cast<float*>(ref)));
-}
-
-void* castToDouble(void* ref) {
-    return new double(static_cast<double>(*static_cast<double*>(ref)));
-}
-
-void* castToChar(void* ref) {
-    return new char(static_cast<char>(*static_cast<char*>(ref)));
+template<>
+void*	cast<DOUBLE>(e_type srcType, void* ref) {
+    return new double(getDoubleValue(srcType, ref));
 }
 
 typedef struct s_final {
 
 	e_type		type;
-	void		*data = NULL;
+	void		*data;
 	std::string strRep;
-	void*		(*cast)(void*);
+
+	void*	castValue(e_type srcType, void *ref) const {
+		switch (type) {
+			case CHAR:
+				return cast<CHAR>(srcType, ref);
+			case INT:
+				return cast<INT>(srcType, ref);
+			case FLOAT:
+				return cast<FLOAT>(srcType, ref);
+			case DOUBLE:
+				return cast<DOUBLE>(srcType, ref);
+			default:
+				return ( NULL );
+		}
+	};
 
 	std::string toStringValue() const {
         switch (type) {
             case CHAR:
-                return testToString<CHAR>(data);
+                return toString<CHAR>(data);
             case INT:
-                return testToString<INT>(data);
+                return toString<INT>(data);
             case FLOAT:
-                return testToString<FLOAT>(data);
+                return toString<FLOAT>(data);
             case DOUBLE:
-                return testToString<DOUBLE>(data);
+                return toString<DOUBLE>(data);
             default:
                 return "undefined";
         }
     }
-} t_final;
-
-#include <limits.h>
+}	t_final;
 
 void ScalarConverter::convert( std::string literal) {
 
-	e_type	type = identify_type(literal);
+	e_type		typeFound = identify_type(literal);
 	
-	char	asChar;
-	long int		asInt;
-	float	asFloat;
-	double	asDouble;
+	long int	asLong;
+	float		asFloat;
+	double		asDouble;
+	t_final		final[4];
 
-	std::string	strRep[4];
-
-	t_final final[4];
 	for ( int i = 0; i < UNDEFINED; i++ )
 		final[i].type = static_cast<e_type>(i);
-	final[INT].cast = &castToInt;
-	final[CHAR].cast = &castToChar;
-	final[FLOAT].cast = &castToFloat;
-	final[DOUBLE].cast = &castToDouble;
-
 
 	// TODO 
 	// - Check the results and manage errors
 	// - Manage pseudo-literals
 	errno = 0;
-	switch (type) {
+	switch (typeFound) {
 		case CHAR:
-			final[CHAR].data = &literal[0];void* (*castFunc)(void*);
+			final[CHAR].data = new char(literal[0]);
 			break;
 		case INT:
-			asInt = std::strtol(literal.c_str(), NULL, 10);
-			final[INT].data = &asInt;
-			if (errno == EINVAL) {
-			 // TODO 
+			asLong = std::strtol(literal.c_str(), NULL, 10);
+			if ( asLong > INT_MAX || asLong < INT_MIN ) {
+				errno = 1;
+				break;
 			}
+			final[INT].data = new int(asLong);
 			break;
 		case FLOAT:
 			asFloat = std::strtof(literal.c_str(), NULL);
-			final[FLOAT].data = &asFloat;
-			if (errno == EINVAL) {
-			  // TODO
-			}
+			final[FLOAT].data = new float(asFloat);
 			break;
 		case DOUBLE:
 			asDouble = std::strtod(literal.c_str(), NULL);
-			final[DOUBLE].data = &asDouble;
-		  	if (errno == EINVAL) {
-		  	    // TODO
-		  	}
+			final[DOUBLE].data = new double(asDouble);
 		  	break;
 		default:
 			break;
 	}
 
-	if ( type != UNDEFINED) {
-
-		for (int i = 0; i < UNDEFINED; i++) {
-			if ( i != type )
-				final[i].data = final[i].cast(final[type].data);
-			final[i].strRep = final[i].toStringValue();
-		}
-	}
-	else {
+	if ( errno || typeFound == UNDEFINED) {
 		for (int i = 0; i < UNDEFINED; i++) {
 			final[i].strRep = "impossible";
 		}
 	}
-	// TODO - Move the inner case in a template function
-	// switch (type) {
-	// 	case CHAR:
-	// 		strRep[CHAR] = toString(asChar);
-	// 		asInt = static_cast<int> ( asChar );
-	// 		strRep[INT] = toString(asInt);
-	// 		asFloat = static_cast<float> ( asChar );
-	// 		strRep[FLOAT] = toString(asFloat);
-	// 		asDouble = static_cast<double>( asChar );
-	// 		strRep[DOUBLE] = toString(asDouble);
-	// 		break;
-	// 	case INT:
-	// 		strRep[INT] = toString(asInt);
-	// 		// TODO Isolate this logic into a function. Maybe a template can manage the whole conversion??
-	// 		if ( asInt < 0 || asInt > 128 )
-	// 			strRep[CHAR] = "impossible";
-	// 		else {
-	// 			asChar = static_cast<char> ( asInt );
-	// 			strRep[CHAR] = toString(asChar);
-	// 		} // End of the todo's logic
-	// 		asFloat = static_cast<float> ( asInt );
-	// 		strRep[FLOAT] = toString(asFloat);
-	// 		asDouble = static_cast<double>( asInt );
-	// 		strRep[DOUBLE] = toString(asDouble);
-	// 		break;
-	// 	case FLOAT:
-	// 		strRep[FLOAT] = toString(asFloat);
-	// 		asChar = static_cast<char> ( asFloat );
-	// 		strRep[CHAR] = toString(asChar);
-	// 		asInt = static_cast<int> ( asFloat );
-	// 		strRep[INT] = toString(asInt);
-	// 		asDouble = static_cast<double>( asFloat );
-	// 		strRep[DOUBLE] = toString(asDouble);
-	// 		break;
-	// 	case DOUBLE:
-	// 		strRep[DOUBLE] = toString(asDouble);
-	// 		asChar = static_cast<char> ( asDouble );
-	// 		strRep[CHAR] = toString(asChar);
-	// 		asInt = static_cast<int> ( asDouble );
-	// 		strRep[INT] = toString(asInt);
-	// 		asFloat = static_cast<float> ( asDouble );
-	// 		strRep[FLOAT] = toString(asFloat);
-	// 		break;
-	// 	default:
-	// 		for (int i = 0; i < UNDEFINED; i++)
-	// 			strRep[i] = "impossible";
-	// 		break;
-	// }
+	else {
+		for (int i = 0; i < UNDEFINED; i++) {
+			if ( i != typeFound )
+				final[i].data = final[i].castValue(typeFound, final[typeFound].data);
+			final[i].strRep = final[i].toStringValue();
+		}
+	}
 
 	std::cout <<
 	"char: " << final[CHAR].strRep << std::endl <<
