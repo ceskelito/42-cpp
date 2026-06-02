664f4c38068811e445c499ae8d18de59201bce5c
Riccardo Ceschel <rceschel@student.42roma.it>
2026-06-02 13:00:31 +0200

    I love to abstract the logic =[] --- maybe am i masochist

--- Module06/ex00/ScalarConverter.class.cpp
+++ Module06/ex00/ScalarConverter.class.cpp
@@ -50,7 +50,7 @@ static bool isDouble( std::string l ) {
 	if (l[0] == '-')
 		i = 1;
 
-	for (; i < l.length(); i++) {
+	for (; i < l.length(); i++) {cpp
 		if ( l[i] == '.' ) {
 			if ( hasDecimal )
 				return false;
@@ -115,103 +115,151 @@ std::string toString<char>(const char& value) {
     return oss.str();
 }
 
+// Funzioni wrapper non-template per ogni tipo
+void* castToInt(void* ref) {
+    return new int(static_cast<int>(*static_cast<int*>(ref)));
+}
+
+void* castToFloat(void* ref) {
+    return new float(static_cast<float>(*static_cast<float*>(ref)));
+}
+
+void* castToDouble(void* ref) {
+    return new double(static_cast<double>(*static_cast<double*>(ref)));
+}
+
+void* castToChar(void* ref) {
+    return new char(static_cast<char>(*static_cast<char*>(ref)));
+}
+
+typedef struct s_final {
+
+	e_type		type;
+	void		*data = NULL;
+	std::string strRep;
+	void*		(*cast)(void*);
+} t_final;
+
+#include <limits.h>
+
 void ScalarConverter::convert( std::string literal) {
 
 	e_type	type = identify_type(literal);
 	
 	char	asChar;
-	int		asInt;
+	long int		asInt;
 	float	asFloat;
 	double	asDouble;
 
 	std::string	strRep[4];
 
+	t_final final[4];
+	for ( int i = 0; i < UNDEFINED; i++ )
+		final[i].type = static_cast<e_type>(i);
+	final[INT].cast = &castToInt;
+	final[CHAR].cast = &castToChar;
+	final[FLOAT].cast = &castToFloat;
+	final[DOUBLE].cast = &castToDouble;
+
+
 	// TODO 
 	// - Check the results and manage errors
 	// - Manage pseudo-literals
+	errno = 0;
 	switch (type) {
 		case CHAR:
-		  asChar = literal[0];
-		  break;
+			final[CHAR].data = &literal[0];
+			break;
 		case INT:
-		  asInt = std::strtol(literal.c_str(), NULL, 10);
-		  if (errno == EINVAL) {
+			asInt = std::strtol(literal.c_str(), NULL, 10);
+			final[INT].data = &asInt;
+			if (errno == EINVAL) {
 			 // TODO 
-		  }
-		  break;
+			}
+			break;
 		case FLOAT:
-		  asFloat = std::strtof(literal.c_str(), NULL);
-		  if (errno == EINVAL) {
+			asFloat = std::strtof(literal.c_str(), NULL);
+			final[FLOAT].data = &asFloat;
+			if (errno == EINVAL) {
 			  // TODO
-		  }
-		  break;
+			}
+			break;
 		case DOUBLE:
-		  asDouble = std::strtod(literal.c_str(), NULL);
-		  if (errno == EINVAL) {
-			  // TODO
-		  }
-		  break;
+			asDouble = std::strtod(literal.c_str(), NULL);
+			final[DOUBLE].data = &asDouble;
+		  	if (errno == EINVAL) {
+		  	    // TODO
+		  	}
+		  	break;
 		default:
-		  break;
+			break;
 	}
 
+	if ( type != UNDEFINED) {
+
+		for (int i = 0; i < UNDEFINED; i++) {
+			if ( i != type )
+				final[i].data = final[i].cast(final[type].data);
+			final[i].strRep = toString(final[type]);
+		}
+	}
+	else {
+		for (int i = 0; i < UNDEFINED; i++) {
+			final[i].strRep = "impossible";
+		}
+	}
 	// TODO - Move the inner case in a template function
-	switch (type) {
-		case CHAR:
-			strRep[CHAR] = toString(asChar);
-			asInt = static_cast<int> ( asChar );
-			strRep[INT] = toString(asInt);
-			asFloat = static_cast<float> ( asChar );
-			strRep[FLOAT] = toString(asFloat);
-			asDouble = static_cast<double>( asChar );
-			strRep[DOUBLE] = toString(asDouble);
-			break;
-		case INT:
-			strRep[INT] = toString(asInt);
-			// TODO Isolate this logic into a function. Maybe a template can manage the whole conversion??
-			if ( asInt < 0 || asInt > 128 )
-				strRep[CHAR] = "impossible";
-			else {
-				asChar = static_cast<char> ( asInt );
-				strRep[CHAR] = toString(asChar);
-			} // End of the todo's logic
-			asFloat = static_cast<float> ( asInt );
-			strRep[FLOAT] = toString(asFloat);
-			asDouble = static_cast<double>( asInt );
-			strRep[DOUBLE] = toString(asDouble);
-			break;
-		case FLOAT:
-			strRep[FLOAT] = toString(asFloat);
-			asChar = static_cast<char> ( asFloat );
-			strRep[CHAR] = toString(asChar);
-			asInt = static_cast<int> ( asFloat );
-			strRep[INT] = toString(asInt);
-			asDouble = static_cast<double>( asFloat );
-			strRep[DOUBLE] = toString(asDouble);
-			break;
-		case DOUBLE:
-			strRep[DOUBLE] = toString(asDouble);
-			asChar = static_cast<char> ( asDouble );
-			strRep[CHAR] = toString(asChar);
-			asInt = static_cast<int> ( asDouble );
-			strRep[INT] = toString(asInt);
-			asFloat = static_cast<float> ( asDouble );
-			strRep[FLOAT] = toString(asFloat);
-			break;
-		default:
-			for (int i = 0; i < UNDEFINED; i++)
-				strRep[i] = "impossible";
-			break;
-	}
+	// switch (type) {
+	// 	case CHAR:
+	// 		strRep[CHAR] = toString(asChar);
+	// 		asInt = static_cast<int> ( asChar );
+	// 		strRep[INT] = toString(asInt);
+	// 		asFloat = static_cast<float> ( asChar );
+	// 		strRep[FLOAT] = toString(asFloat);
+	// 		asDouble = static_cast<double>( asChar );
+	// 		strRep[DOUBLE] = toString(asDouble);
+	// 		break;
+	// 	case INT:
+	// 		strRep[INT] = toString(asInt);
+	// 		// TODO Isolate this logic into a function. Maybe a template can manage the whole conversion??
+	// 		if ( asInt < 0 || asInt > 128 )
+	// 			strRep[CHAR] = "impossible";
+	// 		else {
+	// 			asChar = static_cast<char> ( asInt );
+	// 			strRep[CHAR] = toString(asChar);
+	// 		} // End of the todo's logic
+	// 		asFloat = static_cast<float> ( asInt );
+	// 		strRep[FLOAT] = toString(asFloat);
+	// 		asDouble = static_cast<double>( asInt );
+	// 		strRep[DOUBLE] = toString(asDouble);
+	// 		break;
+	// 	case FLOAT:
+	// 		strRep[FLOAT] = toString(asFloat);
+	// 		asChar = static_cast<char> ( asFloat );
+	// 		strRep[CHAR] = toString(asChar);
+	// 		asInt = static_cast<int> ( asFloat );
+	// 		strRep[INT] = toString(asInt);
+	// 		asDouble = static_cast<double>( asFloat );
+	// 		strRep[DOUBLE] = toString(asDouble);
+	// 		break;
+	// 	case DOUBLE:
+	// 		strRep[DOUBLE] = toString(asDouble);
+	// 		asChar = static_cast<char> ( asDouble );
+	// 		strRep[CHAR] = toString(asChar);
+	// 		asInt = static_cast<int> ( asDouble );
+	// 		strRep[INT] = toString(asInt);
+	// 		asFloat = static_cast<float> ( asDouble );
+	// 		strRep[FLOAT] = toString(asFloat);
+	// 		break;
+	// 	default:
+	// 		for (int i = 0; i < UNDEFINED; i++)
+	// 			strRep[i] = "impossible";
+	// 		break;
+	// }
 
-	// std::cout <<
-	// 	"char: " << '\'' << asChar << '\'' << std::endl <<
-	// 	"int: " << asInt << std::endl <<
-	// 	"float: " << asFloat << 'f' << std::endl <<
-	// 	"double: " << asDouble << std::endl;
 	std::cout <<
-		"char: " << strRep[CHAR] << std::endl <<
-		"int: " << strRep[INT] << std::endl <<
-		"float: " << strRep[FLOAT] << std::endl <<
-		"double: " << strRep[DOUBLE] << std::endl;
+	"char: " << final[CHAR].strRep << std::endl <<
+	"int: " << final[INT].strRep<< std::endl <<
+	"float: " << final[FLOAT].strRep << std::endl <<
+	"double: " << final[DOUBLE].strRep << std::endl;
 }
