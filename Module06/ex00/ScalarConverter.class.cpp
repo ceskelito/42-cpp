eac127178d48d1953d0dd39dcb4ca5e892bee8b0
Riccardo Ceschel <rceschel@student.42roma.it>
2026-06-04 11:49:29 +0200

    removed comments
    Simplified a lot

--- Module06/ex00/ScalarConverter.class.cpp
+++ Module06/ex00/ScalarConverter.class.cpp
@@ -2,67 +2,55 @@
 #include "ScalarConverter.utils.hpp"
 #include <iostream>
 #include <cstdlib>
-#include <errno.h>
 #include <limits.h>
 
-void ScalarConverter::convert( std::string literal) {
+void ScalarConverter::convert(std::string literal) {
 
-	t_final		types[4];
-	e_type		typeFound = identify_type(literal);
+	e_type typeFound = identify_type(literal);
+	double parsedValue = 0.0;
+	bool isOverflow = false;
 
-	for ( int i = 0; i < UNDEFINED; i++ )
-		types[i].type = static_cast<e_type>(i);
-
-	// TODO 
-	// - Manage edge cases like void strings or ' ' character
-
-	// Set the value of the type corresponing with @literal
-	errno = 0;
+	// Parse the input value based on type
 	switch (typeFound) {
 		case CHAR:
-			types[CHAR].data = literal[0];
+			parsedValue = literal[0];
 			break;
 		case INT:
-			types[INT].data = std::strtol(literal.c_str(), NULL, 10);
-			errno = ( types[INT].data > INT_MAX || types[INT].data < INT_MIN );
+			parsedValue = std::strtol(literal.c_str(), NULL, 10);
+			isOverflow = (parsedValue > INT_MAX || parsedValue < INT_MIN);
 			break;
 		case FLOAT:
-			types[FLOAT].data = std::strtof(literal.c_str(), NULL);
+			parsedValue = std::strtof(literal.c_str(), NULL);
 			break;
 		case DOUBLE:
-			types[DOUBLE].data = std::strtod(literal.c_str(), NULL);
-		  	break;
+			parsedValue = std::strtod(literal.c_str(), NULL);
+			break;
 		case PSEUDO:
 			break;
 		default:
 			break;
 	}
 
-	
-	/* Setting the others data and string representations */
-	if ( typeFound < UNDEFINED && !errno ) {
-		for (int i = 0; i < UNDEFINED; i++) {
-			if ( i != typeFound )
-				types[i].setData(types[typeFound].data);
-			types[i].setStringRep();
-		}
+	// Output conversions
+	if (typeFound < UNDEFINED && !isOverflow) {
+		std::cout << "char: " << getStringLiteral<CHAR>(parsedValue) << std::endl;
+		std::cout << "int: " << getStringLiteral<INT>(parsedValue) << std::endl;
+		std::cout << "float: " << getStringLiteral<FLOAT>(parsedValue) << std::endl;
+		std::cout << "double: " << getStringLiteral<DOUBLE>(parsedValue) << std::endl;
 	}
-	else if ( typeFound == PSEUDO ) {
-		std::string	const	s = literal.substr(literal.length() - 3, 3);
-
+	else if (typeFound == PSEUDO) {
+		std::string s = literal.substr(literal.length() - 3, 3);
 		if (s == "anf" || s == "nff")
 			literal.erase(literal.size() - 1);
-		for (int i = 0; i < UNDEFINED; i++)
-			types[i].setStringRep(true, literal);
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: " << literal << "f" << std::endl;
+		std::cout << "double: " << literal << std::endl;
 	}
 	else {
-		for (int i = 0; i < UNDEFINED; i++)
-			types[i].strRep = "impossible";
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: impossible" << std::endl;
+		std::cout << "double: impossible" << std::endl;
 	}
-
-	std::cout <<
-	"char: " << types[CHAR].strRep << std::endl <<
-	"int: " << types[INT].strRep<< std::endl <<
-	"float: " << types[FLOAT].strRep << std::endl <<
-	"double: " << types[DOUBLE].strRep << std::endl;
 }
--- Module06/ex00/ScalarConverter.utils.hpp
+++ Module06/ex00/ScalarConverter.utils.hpp
@@ -3,19 +3,9 @@
 #include <string>
 #include <sstream>
 #include <cctype>
+#include <limits.h>
 
 typedef enum { CHAR, INT, FLOAT, DOUBLE, UNDEFINED, PSEUDO } e_type;
-
-typedef struct s_final {
-
-	e_type		type;
-	double		data;
-	std::string strRep;
-
-	void	setData(double const &otherData);
-	void	setStringRep(bool pseudo = false, std::string literal = "");
-
-}	t_final;
 
 // Type identification functions
 bool		isInt(std::string l);
@@ -27,3 +17,54 @@ e_type		identify_type(std::string l);
 // String literal conversion
 template <e_type Type>
 std::string getStringLiteral(double const &data);
+
+// Template specializations
+template <>
+inline std::string getStringLiteral<CHAR>(double const &data)
+{
+	std::ostringstream	oss;
+	char const			value = static_cast<char>(data);
+
+	if (data < CHAR_MIN || data > CHAR_MAX)
+		oss << "impossible";
+	else if (!std::isprint(data))
+		oss << "not displayable";
+	else
+		oss << "'" << value << "'";
+	return oss.str();
+}
+
+template <>
+inline std::string getStringLiteral<INT>(double const &data)
+{
+	std::ostringstream	oss;
+	int const			value = static_cast<int>(data);
+
+	oss << value;
+	return oss.str();
+}
+
+template <>
+inline std::string getStringLiteral<FLOAT>(double const &data)
+{
+	std::ostringstream	oss;
+	float const			value = static_cast<float>(data);
+
+	oss << value;
+	if (value - static_cast<int>(value) == 0)
+		oss << ".0";
+	oss << "f";
+	return oss.str();
+}
+
+template <>
+inline std::string getStringLiteral<DOUBLE>(double const &data)
+{
+	std::ostringstream	oss;
+	double const		value = data;
+
+	oss << value;
+	if (value - static_cast<int>(value) == 0)
+		oss << ".0";
+	return oss.str();
+}
