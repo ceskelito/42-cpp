d15a7935ffb53a4585ce7aa8a5f591b7575b7f18
Riccardo Ceschel <rceschel@student.42roma.it>
2026-06-11 16:18:29 +0200

    Writing main

--- Module08/ex01/main.cpp
+++ Module08/ex01/main.cpp
@@ -1,15 +1,42 @@
 #include "Span.class.hpp"
+#include "AnsiColorCodes.h"
 #include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+
+static void printHeader (
+		std::string const& text,
+		std::string const background = WHTB,
+		std::string const color = BLK,
+		std::ostream &os = std::cout) {
+
+    os << std::string(color) + background + text + CRESET << std::endl;
+}
 
 int main()
 {
-	Span sp = Span(50000);
-	sp.addNumber(1);
-	sp.addNumber(4);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
-	sp.addRange(10000, 999);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+	Span	sp10u(10);
+	Span	sp50k(50 * 1000);
+
+	printHeader("Span of size 10");
+	try {
+		cout << "Add numbers: 1, 4, 8" << endl;
+		sp10u.addNumber(1);
+		sp10u.addNumber(4);
+		sp10u.addNumber(8);
+		cout << "Shortest Span: " << sp10u.shortestSpan() << endl;
+		cout << "Longest Span: " << sp10u.longestSpan() << endl;
+		cout << "Add 10 times the number 15" << endl;
+		sp10u.addRange(10, 15);
+	}
+	catch (std::length_error &e) {
+		cout << "Error: " << e.what() << endl;
+	}
+	
+	sp10u.addRange(10000, 999);
+	std::cout << sp10u.shortestSpan() << std::endl;
+	std::cout << sp10u.longestSpan() << std::endl;
 	return 0;
 }
