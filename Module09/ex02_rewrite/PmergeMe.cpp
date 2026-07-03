3806656ebde8fa15e64c0a41445fac66d2d4a451
Riccardo Ceschel <rceschel@student.42roma.it>
2026-07-03 18:04:49 +0200

    Traits elements of pairs as std::pairs

--- Module09/ex02_rewrite/PmergeMe.cpp
+++ Module09/ex02_rewrite/PmergeMe.cpp
@@ -1,4 +1,5 @@
 #include <deque>
+#include <utility>
 
 // Source: STEP 1 of https://dev.to/emuminov/human-explanation-and-step-by-step-visualisation-of-the-ford-johnson-algorithm-5g91
 
@@ -13,11 +14,15 @@ static dq dividePairsAndSort(dq sequence, unsigned int elementSize = 1) {
 
 	for (dq::iterator it = sequence.begin(); std::distance(it, sequence.end()) >= pairSize; it += pairSize) {
 
-		dq::iterator firstElem_labelA = it + elementSize - 1;
-		dq::iterator secondElem_labelA = it + pairSize - 1;
+		std::pair<dq::iterator, dq::iterator> firstElem = std::make_pair(it, it + elementSize - 1);
+		std::pair<dq::iterator, dq::iterator> secondElem = std::make_pair(it + elementSize, it + 2 * elementSize - 1);
 
-		if (*firstElem_labelA > *secondElem_labelA)
-			std::swap_ranges(it, it + elementSize, it + elementSize);
+
+		// dq::iterator firstElem_labelA = it + elementSize - 1;
+		// dq::iterator secondElem_labelA = it + pairSize - 1;
+
+		if (*firstElem.second > *secondElem.second)
+			std::swap_ranges(firstElem.first, secondElem.first, secondElem.first);
 	}
 	return dividePairsAndSort(sequence, 2 * elementSize);
 }
