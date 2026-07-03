623ae905c2a22677381549f30080788e78f014b6
Riccardo Ceschel <rceschel@student.42roma.it>
2026-07-03 18:19:53 +0200

    Created Range struct

--- Module09/ex02_rewrite/PmergeMe.cpp
+++ Module09/ex02_rewrite/PmergeMe.cpp
@@ -1,5 +1,5 @@
 #include <deque>
-#include <utility>
+#include "Range.hpp"
 
 // Source: STEP 1 of https://dev.to/emuminov/human-explanation-and-step-by-step-visualisation-of-the-ford-johnson-algorithm-5g91
 
@@ -14,14 +14,10 @@ static dq dividePairsAndSort(dq sequence, unsigned int elementSize = 1) {
 
 	for (dq::iterator it = sequence.begin(); std::distance(it, sequence.end()) >= pairSize; it += pairSize) {
 
-		std::pair<dq::iterator, dq::iterator> firstElem = std::make_pair(it, it + elementSize - 1);
-		std::pair<dq::iterator, dq::iterator> secondElem = std::make_pair(it + elementSize, it + 2 * elementSize - 1);
+		Range<dq::iterator>	firstElem = makeRange(it, it + elementSize - 1);
+		Range<dq::iterator>	secondElem = makeRange(it + elementSize, it + pairSize - 1);
 
-
-		// dq::iterator firstElem_labelA = it + elementSize - 1;
-		// dq::iterator secondElem_labelA = it + pairSize - 1;
-
-		if (*firstElem.second > *secondElem.second)
+		if (*firstElem.last > *secondElem.last)
 			std::swap_ranges(firstElem.first, secondElem.first, secondElem.first);
 	}
 	return dividePairsAndSort(sequence, 2 * elementSize);
