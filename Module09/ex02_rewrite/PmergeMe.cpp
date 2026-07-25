efa0dbc8b63200ebd9a8fde698e4e640ee7e6201
ceskelito <ceschelr@gmail.com>
2026-07-25 17:49:00 +0200

    Wrote step 2 and 3 of initializeAndInsert

--- Module09/ex02_rewrite/PmergeMe.cpp
+++ Module09/ex02_rewrite/PmergeMe.cpp
@@ -13,60 +13,60 @@ void	appendRange(dq &s, Range<dq::iterator> r) {
 	s.insert(s.end(), r.first, r.last + 1);
 }
 
-void	initializeAndInsertWrong(dq &sequence, unsigned int elementSize)
-{
-	dq				main, pend;
-
-	// STEP 1: Divide main and pend into groups
-	unsigned int	pairSize = 2 * elementSize;
-	for (dq::iterator it = sequence.begin(); std::distance(it, sequence.end()) >= pairSize; it += pairSize) {
-		
-		Range<dq::iterator>	elementA = makeRange(it, it + elementSize - 1);
-		Range<dq::iterator>	elementB = makeRange(it + elementSize, it + pairSize - 1);
-		
-		if (it == sequence.begin()) 
-			appendRange(main, elementB);
-		else
-			appendRange(pend, elementB);
-
-		appendRange(main, elementA);
-	}
-
-	// Handle remaining elements
-	unsigned int processed_elements = (sequence.size() / pairSize) * pairSize;
-	for (dq::iterator it = sequence.begin() + processed_elements; it != sequence.end(); it++) {
-		main.insert(main.end(), *it);
-	}
-
-	// STEP 2: Insert pend elements into main using Jacobsthal sequence
-	unsigned int jacobsthal_index = 1;
-	unsigned int pend_index = 0;
-
-	std::sort(main.begin(), main.end());
-	
-	while (pend_index < pend.size()) {
-		unsigned int jacobsthal_num = getJacobsthalNumber(jacobsthal_index);
-		unsigned int limit = std::min(jacobsthal_num, (unsigned int)pend.size());
-		
-		for (unsigned int i = limit; i > pend_index && pend_index < pend.size(); i--) {
-			dq::iterator pos = std::lower_bound(main.begin(), main.end(), pend[pend_index]);
-			main.insert(pos, pend[pend_index]);
-			pend_index++;
-		}
-		
-		jacobsthal_index++;
-	}
-
-	// STEP 3: Copy sorted main back to sequence
-	sequence.clear();
-	if (!main.empty())
-		appendRange(sequence, makeRange(main.begin(), main.end() - 1));
-
-	// STEP 4: Recurse with next group size
-	if (elementSize > 1) {
-		initializeAndInsertWrong(sequence, 2 * elementSize);
-	}
-}
+// void	initializeAndInsertWrong(dq &sequence, unsigned int elementSize)
+// {
+// 	dq				main, pend;
+//
+// 	// STEP 1: Divide main and pend into groups
+// 	unsigned int	pairSize = 2 * elementSize;
+// 	for (dq::iterator it = sequence.begin(); std::distance(it, sequence.end()) >= pairSize; it += pairSize) {
+//
+// 		Range<dq::iterator>	elementA = makeRange(it, it + elementSize - 1);
+// 		Range<dq::iterator>	elementB = makeRange(it + elementSize, it + pairSize - 1);
+//
+// 		if (it == sequence.begin()) 
+// 			appendRange(main, elementB);
+// 		else
+// 			appendRange(pend, elementB);
+//
+// 		appendRange(main, elementA);
+// 	}
+//
+// 	// Handle remaining elements
+// 	unsigned int processed_elements = (sequence.size() / pairSize) * pairSize;
+// 	for (dq::iterator it = sequence.begin() + processed_elements; it != sequence.end(); it++) {
+// 		main.insert(main.end(), *it);
+// 	}
+//
+// 	// STEP 2: Insert pend elements into main using Jacobsthal sequence
+// 	unsigned int jacobsthal_index = 1;
+// 	unsigned int pend_index = 0;
+//
+// 	std::sort(main.begin(), main.end());
+//
+// 	while (pend_index < pend.size()) {
+// 		unsigned int jacobsthal_num = getJacobsthalNumber(jacobsthal_index);
+// 		unsigned int limit = std::min(jacobsthal_num, (unsigned int)pend.size());
+//
+// 		for (unsigned int i = limit; i > pend_index && pend_index < pend.size(); i--) {
+// 			dq::iterator pos = std::lower_bound(main.begin(), main.end(), pend[pend_index]);
+// 			main.insert(pos, pend[pend_index]);
+// 			pend_index++;
+// 		}
+//
+// 		jacobsthal_index++;
+// 	}
+//
+// 	// STEP 3: Copy sorted main back to sequence
+// 	sequence.clear();
+// 	if (!main.empty())
+// 		appendRange(sequence, makeRange(main.begin(), main.end() - 1));
+//
+// 	// STEP 4: Recurse with next group size
+// 	if (elementSize > 1) {
+// 		initializeAndInsertWrong(sequence, 2 * elementSize);
+// 	}
+// }
 
 void	initializeAndInsert(dq &sequence, unsigned int elementSize)
 {
@@ -94,36 +94,44 @@ void	initializeAndInsert(dq &sequence, unsigned int elementSize)
 	}
 
 	// STEP 2: Insert pend elements into main using Jacobsthal sequence
-	// TODO reapet this step for every b element in the pend!!
 	unsigned int jacoIndex = 1;
-	unsigned int jacoNum;
 
-	// TODO add boundaries
-	jacoNum = getJacobsthalNumber(jacoIndex);
-	dq::iterator elemEnd = pend.begin() + (jacoNum * elementSize - 1);
-	dq::iterator elemStart = elemEnd - elementSize + 1;
+	while (pend.size()) {
+		unsigned int jacoNum = getJacobsthalNumber(jacoIndex);
+		unsigned int prevJacoNum = (jacoIndex > 1) ? getJacobsthalNumber(jacoIndex - 1) : 0;
 
-	Range<dq::iterator> elementToInsert = makeRange(elemStart, elemEnd);
+		// Insert elements B from jacoNum down to prevJacoNum+1, backwards
+		for (unsigned int b_index = jacoNum; b_index > prevJacoNum; b_index--) {
+			// Calculate start and end indices of B[b_index] in pend array
+			unsigned int start_idx = (b_index - 1) * elementSize;
+			unsigned int end_idx = b_index * elementSize - 1;
 
-	while (true) {
-		dq::iterator comparison = main.begin() + jacoNum * elementSize - 1;
-		if (*comparison > *elementToInsert.last) {
-			//insert element
-			break;
+			// Check boundaries
+			if (end_idx >= pend.size() - 1)
+				continue;
+
+			// Create Range for this B element
+			Range<dq::iterator> elementB = makeRange(pend.begin() + start_idx,
+													   pend.begin() + end_idx);
+			dq::iterator pos = main.begin() + end_idx;
+			while (*pos > *elementB.last)
+				pos -= elementSize;
+
+			// Insert the entire range into main (and remove from pend)
+			main.insert(pos, elementB.first, elementB.last + 1);
+			pend.erase(elementB.first, elementB.last);
 		}
-		comparison -= elementSize;
-		// TODO add boundaries
+
+		jacoIndex++;
 	}
 
-	// // STEP 3: Copy sorted main back to sequence
-	// sequence.clear();
-	// if (!main.empty())
-	// 	appendRange(sequence, makeRange(main.begin(), main.end() - 1));
-	//
-	// // STEP 4: Recurse with next group size
-	// if (elementSize > 1) {
-	// 	initializeAndInsert(sequence, 2 * elementSize);
-	// }
+	// STEP 3: Copy sorted main + nonPartecipating back to sequence
+	sequence.clear();
+	sequence.insert(sequence.end(), main.begin(), main.end() - 1);
+	sequence.insert(sequence.end(), nonPartecipating.begin(), nonPartecipating.end() - 1);
+	
+	if (elementSize != 1)
+		initializeAndInsert(sequence, elementSize / 2);
 }
 
 static int dividePairsAndSort(dq &sequence, unsigned int elementSize = 1) {
