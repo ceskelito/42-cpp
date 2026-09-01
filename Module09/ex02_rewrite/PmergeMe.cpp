58e6b6ab9fbf799bfd011f735f641477ca19d582
Riccardo Ceschel <rceschel@student.42roma.it>
2026-09-01 18:46:05 +0200

    Removed merge marks in file

--- Module09/ex02_rewrite/PmergeMe.cpp
+++ Module09/ex02_rewrite/PmergeMe.cpp
@@ -119,7 +119,6 @@ static void initializeMainAndPend(dq &sequence, unsigned int elementSize) {
 
 static void insertPendElement(ElementList &main, ElementList &pend, ElementList::iterator &elementToInsert) {
 
-<<<<<<< HEAD
 	ElementList::iterator	boundElement;
 	ElementList::iterator	previousElement;
 
@@ -139,26 +138,6 @@ static void insertPendElement(ElementList &main, ElementList &pend, ElementList:
 		--boundElement;
 	}
 	main.insert(boundElement, *elementToInsert);
-=======
-	ElementList::iterator	insertPosition;
-	ElementList::iterator	previousPosition;
-
-	for (insertPosition = main.begin(); insertPosition != main.end(); ++insertPosition)
-		if (insertPosition->label == 'a' && insertPosition->index == elementToInsert->index)
-			break;
-
-	while (insertPosition != main.begin()) {
-
-		previousPosition = insertPosition - 1;
-
-		g_cmps++;
-		if (previousPosition->value <= elementToInsert->value)
-			break;
-
-		--insertPosition;
-	}
-	main.insert(insertPosition, *elementToInsert);
->>>>>>> bbafb24729ab7879828ef8c4f954bb29eb1fedd9
 	pend.erase(elementToInsert);
 }
 
